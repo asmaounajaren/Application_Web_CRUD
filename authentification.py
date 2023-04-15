d0dc2d59462555b5feccbfabaa61381cc3536e2e
codingirlma <asmaounajaren@gmail.com>
2023-04-15 20:34:42 +0000

    add login form

--- authentification.py
+++ authentification.py
@@ -1,26 +1,47 @@
-from flask import Flask, render_template, flash,url_for,request
-from werkzeug.utils import redirect
-from flask_mysqldb import MySQL
+# from flask import Blueprint, render_template, flash,url_for,request
+# from werkzeug.utils import redirect
+# from flask_mysqldb import MySQL
+# auth =Blueprint("authentification",__name__)
+# auth.secret_key = 'many random bytes'
 
-skills_app =Flask(__name__)
-skills_app.secret_key = 'many random bytes'
-
-skills_app.config['MYSQL_HOST'] = 'localhost'
-skills_app.config['MYSQL_USER'] = 'root'
-skills_app.config['MYSQL_PASSWORD'] = ''
-skills_app.config['MYSQL_DB'] = 'gestioncommande'
-
-mysql = MySQL(skills_app)
-@skills_app.route('/register',methods= ['GET','POST'])
-def ajouter():
-    if request.method == 'POST':
-        flash("bien ajouté avec succès")
-        nom_=request.form['nom']
-        prenom_=request.form['prenom']
-        email_=request.form['email']
-        motdepasse_=request.form['motdepasse']
-        # request.form.get("motdepasse", False)
-        cur = mysql.connection.cursor()
-        cur.execute("insert into utilisateur (nom,prenom,email,motdepasse) values(%s,%s,%s,%s)",(nom_,prenom_,email_,motdepasse_))
-        mysql.connection.commit()
-        return redirect(url_for('home_page'))
+# auth.config['MYSQL_HOST'] = 'localhost'
+# auth.config['MYSQL_USER'] = 'root'
+# auth.config['MYSQL_PASSWORD'] = ''
+# auth.config['MYSQL_DB'] = 'gestioncommande'
+# mysql = MySQL(auth)
+# @auth.route('/register',methods= ['GET','POST'])
+# def register():
+#     if request.method == 'POST':
+#         flash("bien ajouté avec succès")
+#         nom_=request.form['nom']
+#         prenom_=request.form['prenom']
+#         email_=request.form['email']
+#         motdepasse_=request.form['motdepasse']
+#         # request.form.get("motdepasse", False)
+#         cur = mysql.connection.cursor()
+#         cur.execute("insert into utilisateur (nom,prenom,email,motdepasse) values(%s,%s,%s,%s)",(nom_,prenom_,email_,motdepasse_))
+#         mysql.connection.commit()
+#         return redirect(url_for('home_page'))
+    
+# @auth.route('/login', methods=['GET','POST'])
+# def login():
+#     if request.method=='POST':
+#         email=request.form('email')
+#         motdepasse=request.form('motdepasse')
+#          # Check if account exists using MySQL
+#         cursor = mysql.connection.cursor()
+#         cursor.execute('SELECT * FROM utilisateur WHERE email = %s AND motdepasse = %s', (email, motdepasse,))
+#         # Fetch one record and return result
+#         user = cursor.fetchone()
+#                 # If account exists in accounts table in out database
+#         if user:
+#             # Create session data, we can access this data in other routes
+#             session['loggedin'] = True
+#             session['id'] = user['id']
+#             session['username'] = user['username']
+#             # Redirect to home page
+#             return redirect(url_for('home_page'))
+#         else:
+#             # Account doesnt exist or username/password incorrect
+#             flash('Incorrecte email / mot de passe!')
+#             return redirect(url_for('register_page'))
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, flash,url_for,request
+from flask import Flask, render_template, flash,url_for,request,session
 from werkzeug.utils import redirect
 from flask_mysqldb import MySQL
+# from authentification import auth
 
 skills_app =Flask(__name__)
 skills_app.secret_key = 'many random bytes'
+# skills_app.register_blueprint(auth,url_prefix="")
 
 skills_app.config['MYSQL_HOST'] = 'localhost'
 skills_app.config['MYSQL_USER'] = 'root'
 skills_app.config['MYSQL_PASSWORD'] = ''
 skills_app.config['MYSQL_DB'] = 'gestioncommande'
+
+skills_app.config["SESSION_PERMANENT"] = False
+skills_app.config["SESSION_TYPE"] = "filesystem"
+# session(skills_app)
 
 mysql = MySQL(skills_app)
 
@@ -69,5 +75,47 @@ def about_page():
 def register_page():
     return render_template("register.html")
 
+@skills_app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@skills_app.route('/register',methods= ['GET','POST'])
+def register():
+    if request.method == 'POST':
+        flash("bien ajouté avec succès")
+        nom_=request.form['nom']
+        prenom_=request.form['prenom']
+        email_=request.form['email']
+        motdepasse_=request.form['motdepasse']
+        # request.form.get("motdepasse", False)
+        cur = mysql.connection.cursor()
+        cur.execute("insert into utilisateur (nom,prenom,email,motdepasse) values(%s,%s,%s,%s)",(nom_,prenom_,email_,motdepasse_))
+        mysql.connection.commit()
+        return redirect(url_for('home_page'))
+    
+@skills_app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        email=request.form['email']
+        motdepasse=request.form['motdepasse']
+         # Check if account exists using MySQL
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM utilisateur WHERE email = %s AND motdepasse = %s', (email, motdepasse,))
+        # Fetch one record and return result
+        user = cursor.fetchone()
+        # If account exists in accounts table in out database
+        print(user)
+        if user:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = user[0]
+            session['nom'] = user[1]
+            session['prenom'] = user[2]
+            # Redirect to home page
+            return redirect(url_for('home_page'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            flash('email ou mot de passe est incorrect !')
+            return redirect(url_for('login_page'))
 if __name__ == "__main__":
     skills_app.run(debug=True,port=5000)
